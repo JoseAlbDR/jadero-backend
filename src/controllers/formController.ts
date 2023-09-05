a5cb6f13d0ec47d6ce2a17c65219b3295f19f043
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-09-05 12:07:19 +0200

    feat: send contact form email with sendgrid

--- src/controllers/formController.ts
+++ src/controllers/formController.ts
@@ -1,8 +1,22 @@
+import "dotenv/config";
 import { Response } from "express";
 import { IContactFormRequest } from "../types/contactInterfaces";
 import { StatusCodes } from "http-status-codes";
+import sgMail from "@sendgrid/mail";
 
-export const formController = (req: IContactFormRequest, res: Response) => {
-  console.log(req.body);
-  res.status(StatusCodes.OK).json({ data: req.body });
+export const formController = async (
+  req: IContactFormRequest,
+  res: Response
+) => {
+  const { name, email, subject, message } = req.body;
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  const msg = {
+    to: "josealbdr84@gmail.com",
+    from: "jaderodev@gmail.com",
+    subject: subject,
+    text: "Portfolio Contact Form",
+    html: `<h2>Email from ${name}, ${email}</h2><p>${message}</p>`,
+  };
+  const response = await sgMail.send(msg);
+  res.status(StatusCodes.OK).json(response);
 };
